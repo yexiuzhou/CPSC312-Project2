200e958f76a67a96fcecf0f964e3f0de790fd1f2
Isaac Cheung <30461770+cheungis@users.noreply.github.com>
2019-11-30 17:13:45 -0800

    Add files via upload

--- planner.pl
+++ planner.pl
@@ -1,3 +1,28 @@
-% Actual degree planner/prerequisite checker
+% Actual degree planner
 
-:- include('courseDataBase.pl').
+:- include('transcript.pl').
+
+% graduated(T) is true if the Transcript T given will allow you to graduate.
+graduated(T) :-
+    complete(T, _, _, _, _, _).
+
+
+% complete(T,Communications,Lab,Art,Core,UpperCS) is True if
+% Transcript T fulfills the 8 requirements.
+complete(T,Communications,Lab,Art,Core,UpperCS) :-
+	communicationsRequirement(T,T1),
+	isDifferent(T,Communications,T1),
+	lab_requirements(T1,T2),
+	isDifferent(T1,Lab,T2),
+	artRequirements(T2,T3),
+	isDifferent(T2,Art,T3),
+	coreRequirements(T3,T4),
+	isDifferent(T3,Core,T4),
+	upperCSRequirements(T4,UpperCS,T5).
+
+
+
+% isDifferent(T1,Requirements,T2) is True if T2 is T1 with requirements removed. 
+isDifferent(T,[],T).
+isDifferent(T1,Requirements,T2) :-
+	subtract(T1,T2,Requirements).
---
+++ transcript.pl
@@ -0,0 +1,6 @@
+% Database for relevant courses
+
+% use below to import file:
+% :- include('courseDataBase.pl').
+
+% CPSC courses
