5450d1bae782e7ea9ba862ed4b43348b490ac15b
isaac cheung <isaac.cheung@hotmail.com>
2019-11-30 18:47:15 -0800

    planner.pl now has comments on what helper functions should do

--- planner.pl
+++ planner.pl
@@ -9,29 +9,46 @@ graduated(T) :-
 
 % complete(T,Communications,Lab,Art,Core,UpperCS) is True if
 % Transcript T fulfills the 8 requirements.
-complete(T,Communications,Lab,Art,Core,UpperCS) :-
+complete(T,Communications,Lab,Core,UpperCS) :-
 	creditCount(T),
 	sciCount(T),
 	upperCount(T),
 	upperScieCount(T),
 	artCount(T),
+	breadth(T),
 	communicationsRequirement(T,T1),
 	isDifferent(T,Communications,T1),
 	lab_requirements(T1,T2),
 	isDifferent(T1,Lab,T2),
-	artRequirements(T2,T3),
-	isDifferent(T2,Art,T3),
-	coreRequirements(T3,T4),
-	isDifferent(T3,Core,T4),
-	upperCSRequirements(T4,UpperCS,T5).
+	coreRequirements(T2,T3),
+	isDifferent(T2,Core,T3),
+	upperCSRequirements(T3,T4),
+	isDifferent(T3,UpperCS,T4).
 
 % creditCount(T) is true if transcript T has 120 credits
 
 % sciCount(T) is true if transcript T has 72 science credits
 
-% upperCount(T) is true if transcript T has 48 credits 300 level+
+% upperCount(T) is true if transcript T has atleast 48 credits 300 level+
 
-% 
+% upperScieCount(T) is true if transcript T has 30 credits 300 level+ 
+% and from the science faculty.
+
+% artCount(T) is true if transcript T has atleast 12 and not more than 18 art credits
+
+% breadth(T) is true if transcript T has atleast 3 credits from 6 of the following course codes
+% math, chem, phys, biol, stat, cpsc, eosc
+
+% communicationsRequirement(T,T1) is true if transcript T has atleast 6 credits from communications 
+% , and T1 is T without the 6 credits from communications.
+
+% lab_requirements(T,T1) is true if transcript T has atleast 3 credits from lab requirements 
+% , and T1 is T without the 3 credits from lab requirements.
+
+% coreRequirements(T,T1) is true if transcript T has 
+
+% upperCSRequirements(T,T1) is true if transcript T has
+
 
 % isDifferent(T1,Requirements,T2) is true if transcript T2 is T1 with 
 % requirements removed. 
