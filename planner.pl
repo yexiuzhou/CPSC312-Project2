9b0fffea0c7ac17fc1f1216123e75052affde2c8
isaac cheung <isaac.cheung@hotmail.com>
2019-12-03 14:01:34 -0800

    uncommented lines that were commented during bugfixing

--- planner.pl
+++ planner.pl
@@ -15,7 +15,7 @@ ask(T, ['can', 'i', 'graduate'], 'yes') :- graduated(T).
 ask(T, ['can', 'i', 'graduate'], 'no') :- \+ graduated(T).
 
 ask(T, ['have', 'i', 'met' | Req], 'yes') :- requirementPhrase(Req, T).
-% ask(T, ['have', 'i', 'met' | Req], 'no') :- \+ requirementPhrase(Req, T).
+ask(T, ['have', 'i', 'met' | Req], 'no') :- \+ requirementPhrase(Req, T).
 
 ask(T, ['did', 'i', 'complete' | Course], 'yes') :- member(Course, T).
 ask(T, ['did', 'i', 'complete' | Course], 'no') :- \+ member(Course, T).
